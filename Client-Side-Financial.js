8568f4148d354e299c95e9a786e1bc5d4f1d90e3
ttefera1 <ttefera1@luc.edu>
2022-04-20 11:40:22 -0500

    main branch

--- Client-Side-Financial.js
+++ Client-Side-Financial.js
@@ -1,1 +1,26 @@
 
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-110%';
+}
+let content = document.getElementById('text');
+let hero = document.querySelector('.hero');
+window.addEventListener('scroll', scrollMove());
+function scrollMove(){
+let value = window.scrollY;
+hero.style.left = value * 0.25 + 'px'
+content.style.marginRight = value * 4 +'px';
+//content.style.marginTop = value * 1.5 +'px';
+}
+
+
