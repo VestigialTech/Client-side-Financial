cf69ac56b8a73253c0b50e6bcab36121147337b5
ttefera1 <ttefera1@luc.edu>
2022-04-24 20:31:58 -0500

    login css and few fix

--- login.js
+++ login.js
@@ -41,6 +41,20 @@ function login() {
 $(document).ready(login);
 /*/
 
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-110%';
+}
 
 class HashTable {
   constructor() {
